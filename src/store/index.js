200dba17ef64caeaf45c554297a3df9321b329a5
Cristina <cmsalvador92@gmail.com>
2022-03-20 18:43:30 +0100

    +peticion API con axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
-import { createStore } from 'vuex'
+import axios from 'axios'
+import Vuex from 'vuex'
+import Vue from 'vue'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+
+Vue.use(Vuex);
+
+const state = {
+    entradas: []
+}
+
+const getters = {}
+
+//to handle actions
+const actions = {
+    getEntradas({ commit }) {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                commit('SET_ENTRADAS', response.data)
+        })
+    }
+}
+
+//to handle mutations
+const mutations = {
+    SET_ENTRADAS(state, entradas) {
+        state.entradas = entradas
+    }
+}
+
+//export store module
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
 })
