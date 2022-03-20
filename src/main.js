ec42b827add9644dcd396fd52cb398c83ab19f7b
ceemeese <85557597+ceemeese@users.noreply.github.com>
2022-03-20 21:34:24 +0100

    Revert "Inicio"

--- src/main.js
+++ src/main.js
@@ -3,10 +3,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './styles/styles.css'
-
-
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app')
+createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,12 @@
 import { createStore } from 'vuex'
-import axios from 'axios'
-
-
 
 export default createStore({
-
-    state: {
-        entradas: [],
-    },
-
-    getters: {
-
-    },
-
-    actions: {
-        getEntradas({ commit }) {
-            axios.get('https://jsonplaceholder.typicode.com/posts')
-                .then(response => {
-                    commit('SET_ENTRADAS', response.data)
-            })
-        }
-    },
-
-    mutations: {
-        SET_ENTRADAS(state, entradas) {
-            state.entradas = entradas
-        }
-    }
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
 })
